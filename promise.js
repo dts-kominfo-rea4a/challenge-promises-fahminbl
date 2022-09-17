897f39fe76d7cd5e6f131d958c4102807d8c9d77
Fahmi Nabil <fahmi7126@smkn1gombong.sch.id>
2022-09-17 18:32:59 +0700

    menambahkan promise yang wadidaw

--- promise.js
+++ promise.js
@@ -1,8 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = (emosi) => {
+  return new Promise( async (resolve) => {
+      try {
+        const ixx = await promiseTheaterIXX();
+        const vgc = await promiseTheaterVGC();
+        let emosiIXX = {};
+        let emosiVGC = {};
+        ixx.forEach((emote) => {
+          emosiIXX[emote.hasil] = (emosiIXX[emote.hasil] || 0) + 1;
+        });
+        vgc.forEach((emote) => {
+          emosiVGC[emote.hasil] = (emosiVGC[emote.hasil] || 0) + 1;
+        });
+        let { marah:marahIXX, 'tidak marah':tidakMarahIXX } = emosiIXX;
+        let { marah:marahVGC, 'tidak marah':tidakMarahVGC} = emosiVGC;
+        if (emosi == 'marah') {
+          const hasil = marahIXX + marahVGC;
+          resolve(hasil);
+        } else {
+          const hasil = tidakMarahIXX + tidakMarahVGC;
+          resolve(hasil);
+        }
+      } catch (err) {
+        throw(err);
+      }
+  });
+};
 module.exports = {
   promiseOutput,
 };
